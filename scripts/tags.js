ba09a222c6459758fd4c87205bbf160d2ff67473
Ryan <iii.ryan@outlook.com>
2019-10-05 22:51:42 +0800

    updated 20191005

--- scripts/tags.js
+++ scripts/tags.js
@@ -2,30 +2,26 @@
 
 'use strict';
 
-hexo.extend.tag.register(
-  'note',
-  function(args, content) {
-    var className = args.shift();
-    var header = '';
-    var result = '';
-    var icon = '';
+hexo.extend.tag.register('note', function(args, content) {
+  var className = args.shift();
+  var header = '';
+  var result = '';
+  var icon = '';
 
-    if (args.length) {
-      header += '<strong class="note-title">' + args.join(' ') + '</strong>';
-    }
+  if (args.length) {
+    header += '<strong class="note-title">' + args.join(' ') + '</strong>';
+  }
 
-    icon +=
+  icon +=
       '<svg class="icon"><use xlink:href="#' +
       className +
       '-icon" href="#' +
       className +
       '-icon"></use></svg>';
 
-    result += '<blockquote class="note ' + className + '">' + icon + header;
-    result += hexo.render.renderSync({ text: content, engine: 'markdown' });
-    result += '</blockquote>';
+  result += '<blockquote class="note ' + className + '">' + icon + header;
+  result += hexo.render.renderSync({ text: content, engine: 'markdown' });
+  result += '</blockquote>';
 
-    return result;
-  },
-  true
-);
+  return result;
+}, true);
